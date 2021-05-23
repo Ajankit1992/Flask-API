2a8286f144cf1d80d494471e37414ad3bc2c774f
Ajankit1992 <38551942+Ajankit1992@users.noreply.github.com>
2021-05-23 15:37:05 +0530

    Add files via upload

--- rest.py
+++ rest.py
@@ -1,3 +1,4 @@
+from re import L
 import pymysql, logging
 from app import app
 from db import mysql
@@ -14,10 +15,10 @@ logging.basicConfig(filename="noteapplog.log", format='%(filename)s: %(message)s
 logger.setLevel(logging.DEBUG)
 		
 @app.route('/add', methods=['POST'])
-def add_user():
+def add_note():
 	conn = None
 	cursor = None
-	logger.info("Note add request coming.")
+	logger.info("Post Request comes for adding note in DB.")
 	try:
 		_json = request.json
 		_note = _json['note']
@@ -31,9 +32,12 @@ def add_user():
 			cursor.execute(sql, data)
 			conn.commit()
 			resp = jsonify('Note added successfully!')
-			resp.status_code = 200
-			logger.info("Note added successfully!")
-			return resp
+			if resp.status_code == 200:
+				logger.info("Note added successfully!")
+				return resp
+			else:
+				logger.error("Note added failure!")
+				return ("Note added failure!")
 		else:
 			logger.error("Note added failure!")
 			return not_found()
@@ -55,30 +59,39 @@ def data():
 		cursor.execute("SELECT user_id id, Note FROM NoteApp")
 		rows = cursor.fetchall()
 		resp = jsonify(rows)
-		resp.status_code = 200
-		logger.info("Data found")
-		return resp
+		if resp.status_code == 200:
+			logger.info("Data found")
+			return resp
+		else:
+			logger.error("Data not found")
+			return ("Data not found")
+
 	except Exception as e:
 		print(e)
 		logger.error("Data not found")
+		
 	finally:
 		cursor.close() 
 		conn.close()
 		
-@app.route('/notedata/<int:id>')
-def notedata(id):
+@app.route('/searchnote/<string:note>')
+def Search_Note(note):
 	conn = None
 	cursor = None
 	logger.info("User request for Get data from db on user id basis")
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT user_id id, Note FROM NoteApp WHERE user_id=%s", id)
-		row = cursor.fetchone()
+		cursor.execute(f"SELECT * FROM NoteApp WHERE note like '%{note}%'")
+		row = cursor.fetchall()
 		resp = jsonify(row)
-		resp.status_code = 200
-		logger.info("Data found")
-		return resp
+		# print(resp.status_code)
+		if resp.status_code == 200:
+			logger.info("Data found")
+			return resp
+		else:
+			logger.error("Data not found")
+			return ("Data not found")
 	except Exception as e:
 		print(e)
 		logger.error("Data not found")
@@ -105,9 +118,13 @@ def update_Note():
 			cursor.execute(sql, data)
 			conn.commit()
 			resp = jsonify('Note updated successfully!')
-			resp.status_code = 200
-			logger.info("Note updated successfully!")
-			return resp
+			print(resp.status_code)
+			if resp.status_code == 200:
+				logger.info("Note updated successfully!")
+				return resp
+			else:
+				logger.error("Note updated failure!")
+				return ("Note updated failure!")
 		else:
 			logger.error("Note updated failure!")
 			return not_found()
@@ -126,12 +143,15 @@ def delete(id):
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor()
-		cursor.execute("DELETE FROM NoteApp WHERE user_id=%s", (id,))
+		cursor.execute("DELETE FROM NoteApp WHERE user_id=%s", id)
 		conn.commit()
 		resp = jsonify('Note deleted successfully!')
-		resp.status_code = 200
-		logger.info("Note deleted successfully!")
-		return resp
+		if resp.status_code == 200:
+			logger.info("Note deleted successfully!")
+			return resp
+		else:
+			logger.error("Note deleted failure!")
+			return ("Note deleted failure!")
 	except Exception as e:
 		print(e)
 		logger.error("Note deleted failure!")
